f5bdbf44adb1daeb9623f269256e41b0c57e9c70
Vladimir Sushkov <volodya.sushkov@gmail.com>
2019-05-05 23:08:50 +0300

    Added mintable/burnable functionality (added only owner restrict). Updated truffle

--- contracts/StockupShareToken.sol
+++ contracts/StockupShareToken.sol
@@ -1,15 +1,19 @@
 pragma solidity ^0.5.2;
 
-import "openzeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol";
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20Pausable.sol";
 
 /**
  * @title StockupShareToken
- * @dev ERC20 Token, mintable, pausable, burnable (self tokens by owner).
- * Token represents shares of company.
+ * @dev ERC20 Token:
+ * - has owner
+ * - mintable (by owner)
+ * - burnable (by owner)
+ * - pausable
+ * Token represents shares of company on stockup platform.
  */
-contract StockupShareToken is ERC20Detailed, ERC20Mintable, ERC20Pausable {
+contract StockupShareToken is ERC20Detailed, ERC20Pausable, Ownable {
     uint8 public constant DECIMALS = 0;
 
     /**
@@ -28,10 +32,21 @@ contract StockupShareToken is ERC20Detailed, ERC20Mintable, ERC20Pausable {
     }
 
     /**
+     * @dev Function to mint tokens
+     * @param to The address that will receive the minted tokens.
+     * @param value The amount of tokens to mint.
+     * @return A boolean that indicates if the operation was successful.
+     */
+    function mint(address to, uint256 value) external onlyOwner returns (bool) {
+        _mint(to, value);
+        return true;
+    }
+
+    /**
      * @dev Burns a specific amount of tokens from minter address.
      * @param value The amount of token to be burned.
      */
-    function burn(uint256 value) external onlyMinter {
+    function burn(uint256 value) external onlyOwner {
         _burn(msg.sender, value);
     }
 }
--- test/StockupShareToken.test.js
+++ test/StockupShareToken.test.js
@@ -1,14 +1,16 @@
-/* global artifacts contract context it beforeEach */
-const { BN, shouldFail } = require('openzeppelin-test-helpers');
+/* global artifacts contract describe context it beforeEach */
+const { BN, shouldFail, constants, expectEvent } = require('openzeppelin-test-helpers');
+
+const { ZERO_ADDRESS } = constants;
 
 const StockupShareToken = artifacts.require('StockupShareToken');
 
-contract('StockupShareToken', function([creator, minter, anyone]) {
+contract('StockupShareToken', function([owner, anyone]) {
   const NAME = 'CompanyShareToken';
   const SYMBOL = 'CST';
 
   beforeEach(async function() {
-    this.token = await StockupShareToken.new(NAME, SYMBOL, { from: creator });
+    this.token = await StockupShareToken.new(NAME, SYMBOL, { from: owner });
   });
 
   it('has a name', async function() {
@@ -27,32 +29,83 @@ contract('StockupShareToken', function([creator, minter, anyone]) {
     (await this.token.totalSupply()).should.be.bignumber.equal(new BN(0));
   });
 
-  context('with minted tokens to minter', function() {
+  describe('mint tokens', function() {
+    beforeEach(async function() {
+      this.value = new BN(5);
+    });
+
+    it('should mint tokens by owner', async function() {
+      await this.token.mint(owner, this.value, { from: owner });
+    });
+
+    it('reverts on mint tokens by anyone', async function() {
+      await shouldFail.reverting(this.token.mint(anyone, this.value, { from: anyone }));
+    });
+
+    it('requires non-zero tokens beneficiary', async function() {
+      await shouldFail.reverting(this.token.mint(ZERO_ADDRESS, this.value, { from: owner }));
+    });
+
+    context('when tokens was minted', function() {
+      beforeEach(async function() {
+        ({ logs: this.logs } = await this.token.mint(owner, this.value, { from: owner }));
+      });
+
+      it('should mints the requested amount', async function() {
+        (await this.token.balanceOf(owner)).should.be.bignumber.equal(this.value);
+      });
+
+      it('should log mint-transfer event', async function() {
+        expectEvent.inLogs(this.logs, 'Transfer', {
+          from: ZERO_ADDRESS,
+          to: owner,
+          value: this.value,
+        });
+      });
+    });
+  });
+
+  describe('burn tokens', function() {
     beforeEach(async function() {
       this.value = new BN(10);
 
-      await this.token.addMinter(minter, { from: creator });
-      await this.token.renounceMinter({ from: creator });
-
-      await this.token.mint(minter, this.value, { from: minter });
+      await this.token.mint(owner, this.value, { from: owner });
     });
 
-    it('should burn tokens by minter', async function() {
-      await this.token.burn(this.value, { from: minter });
+    it('should burn tokens by owner', async function() {
+      await this.token.burn(this.value, { from: owner });
     });
 
     it('reverts burn tokens by anyone', async function() {
       await shouldFail.reverting(this.token.burn(this.value, { from: anyone }));
     });
 
+    context('when tokens was burned', function() {
+      beforeEach(async function() {
+        ({ logs: this.logs } = await this.token.burn(this.value, { from: owner }));
+      });
+
+      it('should burns the requested amount', async function() {
+        (await this.token.balanceOf(owner)).should.be.bignumber.equal(new BN(0));
+      });
+
+      it('should log burn-transfer event', async function() {
+        expectEvent.inLogs(this.logs, 'Transfer', {
+          from: owner,
+          to: ZERO_ADDRESS,
+          value: this.value,
+        });
+      });
+    });
+
     context('when anyone has tokens', function() {
       beforeEach(async function() {
         this.anyoneValue = new BN(1);
 
-        await this.token.transfer(anyone, this.anyoneValue, { from: minter });
+        await this.token.transfer(anyone, this.anyoneValue, { from: owner });
       });
 
-      it('reverts burn tokens by anyone', async function() {
+      it('reverts burn self tokens', async function() {
         await shouldFail.reverting(this.token.burn(this.anyoneValue, { from: anyone }));
       });
     });
