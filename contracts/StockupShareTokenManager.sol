723c8d62d06a45df35e807d05ca6760ccc161b5e
Vladimir Sushkov <volodya.sushkov@gmail.com>
2019-05-07 16:53:14 +0300

    Added investors whitelist in token manager

--- contracts/StockupShareTokenManager.sol
+++ contracts/StockupShareTokenManager.sol
@@ -1,11 +1,47 @@
 pragma solidity ^0.5.2;
 
+import "./access/IssuerOwnerRoles.sol";
+
 /**
  * @title StockupShareTokenManager
  * @dev Contract for managing a share-token.
  */
-contract StockupShareTokenManager {
-    constructor() public {
+contract StockupShareTokenManager is IssuerOwnerRoles {
+    // Investors whitelist
+    mapping (address => bool) private _whitelist;
 
+    /**
+    * @dev Constructor.
+    * @param issuer The address of issuer account
+    */
+    constructor(
+        address issuer
+    )
+        public IssuerOwnerRoles(issuer)
+    {
+
+    }
+
+    /**
+     * @return is the investor in the whitelist.
+     */
+    function isWhitelisted(address account) public view returns (bool) {
+        return _whitelist[account];
+    }
+
+    /**
+     * @dev Adds single address to whitelist.
+     * @param account Address to be added to the whitelist
+     */
+    function addToWhitelist(address account) public onlyIssuerOrOwner {
+        _whitelist[account] = true;
+    }
+
+    /**
+     * @dev Removes single address from whitelist.
+     * @param account Address to be removed to the whitelist
+     */
+    function removeFromWhitelist(address account) public onlyIssuerOrOwner {
+        _whitelist[account] = false;
     }
 }
